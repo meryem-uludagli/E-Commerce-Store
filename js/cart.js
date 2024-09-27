edfb507432fa015bcc4528c4cc1859479ca32236
meryem-uludagli <meryemuslu2035@gmail.com>
2024-09-27 13:18:12 +0300

    updated

--- js/cart.js
+++ js/cart.js
@@ -77,11 +77,24 @@ export const renderCartItems = () => {
     removeButton.addEventListener("click", removeFromCart);
   }
   const quantityInputs = document.getElementsByClassName("cart-item-quantity");
+
   for (let i = 0; i < quantityInputs.length; i++) {
     const quantityInput = quantityInputs[i];
-    quantityInput.addEventListener("change", () => {
-      console.log(`İnput Çalıştı`);
-    });
+    quantityInput.addEventListener("change", onQuantityChange);
+  }
+};
+
+const onQuantityChange = (event) => {
+  const newQuantity = +event.target.value;
+  const productId = +event.target.dataset.id;
+
+  if (newQuantity > 0) {
+    const cartItem = cart.find((item) => item.id === productId);
+    if (!cartItem) return;
+    cartItem.quantity = newQuantity;
+    saveToLocalStorage(cart);
+    updateCartIcon(cart);
+    displayCartTotal();
   }
 };
 
